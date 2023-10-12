a275ff354a2a287944feeb5d3619e565420cf93a
kahirokunn <okinakahiro@gmail.com>
2023-10-12 17:50:22 +0900

    chore: Add generate-all-k8s-client example

---
+++ examples/generate-all-k8s-client/codegen.ts
@@ -0,0 +1,86 @@
+import { $ } from 'https://deno.land/x/zx_deno/mod.mjs'
+import * as path from 'https://deno.land/std/path/mod.ts'
+
+type Path = string
+type Url = string
+
+type SwaggerV3 = {
+  paths: Record<
+    Path,
+    {
+      serverRelativeURL: Url
+    }
+  >
+}
+
+const getTsFileName = (path: string) => {
+  const replacedStr = path.replace(/[\/.]/g, '-')
+  return `${
+    replacedStr.startsWith('-') ? replacedStr.substring(1) : replacedStr
+  }.ts`
+}
+
+const res = await $`kubectl get --raw /openapi/v3`
+const source = JSON.parse(res.toString()) as SwaggerV3
+
+const genK8sClientCodegenConfigSourceCode = (
+  swaggerFilePath: string,
+  apiFile: string,
+  outputFilePath: string
+) =>
+  `import type { ConfigFile } from 'kubernetes-typescript-client-codegen-openapi'
+
+const config: ConfigFile = {
+  schemaFile: '${swaggerFilePath}',
+  apiFile: '${apiFile}',
+  outputFile: '${outputFilePath}',
+}
+
+export default config
+`
+const cwd = Deno.cwd()
+
+const k8sClientCodegenConfigDirectoryPath = path.join(
+  cwd,
+  '/k8s-client/generated/codegen-config'
+)
+const k8sClientDirectoryPath = path.join(cwd, 'k8s-client/generated/client')
+await $`mkdir -p ${k8sClientCodegenConfigDirectoryPath}`
+await $`mkdir -p ${k8sClientDirectoryPath}`
+
+await Promise.all(
+  Object.keys(source.paths).map(async (sourcePath) => {
+    const swaggerFilePath = path.join(
+      cwd,
+      'tmp/api/openapi-spec/v3',
+      sourcePath,
+      'swagger.json'
+    )
+    const k8sClientCodegenConfigFilePath = path.join(
+      k8sClientCodegenConfigDirectoryPath,
+      getTsFileName(sourcePath)
+    )
+    const k8sClientFilePath = path.join(
+      k8sClientDirectoryPath,
+      getTsFileName(sourcePath)
+    )
+    await $`mkdir -p $(dirname ${swaggerFilePath})`
+    const res = await $`kubectl get --raw /openapi/v3/${sourcePath}`
+    await Deno.writeTextFile(swaggerFilePath, res.toString())
+    await Deno.writeTextFile(
+      k8sClientCodegenConfigFilePath,
+      genK8sClientCodegenConfigSourceCode(
+        path.relative(k8sClientCodegenConfigDirectoryPath, swaggerFilePath),
+        path.join(
+          path.relative(
+            k8sClientCodegenConfigDirectoryPath,
+            path.join(cwd, 'k8s-client')
+          ),
+          'client.ts'
+        ),
+        path.relative(k8sClientCodegenConfigDirectoryPath, k8sClientFilePath)
+      )
+    )
+    await $`npx kubernetes-typescript-client-codegen-openapi ${k8sClientCodegenConfigFilePath}`
+  })
+)
---
+++ examples/generate-all-k8s-client/patch-kube-config.ts
@@ -0,0 +1,40 @@
+import { parse, stringify } from 'https://deno.land/std/yaml/mod.ts'
+import { existsSync } from 'https://deno.land/std/fs/mod.ts'
+
+type Env = {
+  name: string
+  value: string
+}
+
+type User = {
+  exec?: {
+    apiVersion: string
+    args: string[]
+    command: string
+    env?: Env[]
+  }
+}
+
+type KubeConfig = {
+  users: { name: string; user: User }[]
+}
+
+const defaultKubeConfigPath = Deno.env.get('HOME') + '/.kube/config'
+
+const kubeConfigPath = Deno.env.get('KUBECONFIG') || defaultKubeConfigPath
+if (existsSync(kubeConfigPath)) {
+  const kubeConfig = parse(
+    await Deno.readTextFile(kubeConfigPath)
+  ) as KubeConfig
+  kubeConfig.users.forEach(({ user }) => {
+    const AWS_PROFILE_ENV = user.exec?.env?.find(
+      (e) => e.name === 'AWS_PROFILE'
+    )
+    const HAS_PROFILE_ARG = user.exec?.args?.find((arg) => arg === '--profile')
+    if (AWS_PROFILE_ENV && !HAS_PROFILE_ARG) {
+      user.exec?.args.push('--profile')
+      user.exec?.args.push(AWS_PROFILE_ENV.value)
+    }
+  })
+  await Deno.writeTextFile(kubeConfigPath, stringify(kubeConfig))
+}
