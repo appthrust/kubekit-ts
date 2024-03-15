27fa005bae3946ac27e04a55b56bf650d3486629
kahirokunn <okinakahiro@gmail.com>
2024-03-15 17:30:56 +0900

    テスト用にnextjsを配置します

---
+++ examples/generate-all-k8s-client/tests/my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { listCoreV1PodForAllNamespaces } from '../../../../k8s-client/generated/client/api-v1'
+
+export default async function Home() {
+  const pods = await listCoreV1PodForAllNamespaces({})
+  return (
+    <main>
+      <ul>
+        {pods.items.map((pod, i) => <li key={i}>{pod.metadata?.name}</li>)}
+      </ul>
+    </main>
+  );
+}
