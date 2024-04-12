505674c0f4f50e67523942bd68b2a052cc766be8
kahirokunn <okinakahiro@gmail.com>
2024-04-13 00:52:11 +0900

    * apiVersionとkindをstring literalで正確な値しか受け付けなくしました * server side applyの場合、apiVersionとkindが必須なので、requiredになるようにしました.

--- packages/kubekit-sync/src/bin/patchOpenApi/index.ts
+++ packages/kubekit-sync/src/bin/patchOpenApi/index.ts
@@ -1,10 +1,10 @@
-import { OpenAPIV3 } from "openapi-types";
-import { patchOp } from "./op";
-import { patchRequiredFields } from "./requiredFields";
+import { OpenAPIV3 } from 'openapi-types'
+import { patchOp } from './op'
+import { patchApplyPatch } from './applyPatch'
+import { patchGvk } from './gvk'
 
 export const patchFunctions = [
   patchOp,
-  patchRequiredFields,
-] satisfies Array<
-  (doc: OpenAPIV3.Document) => OpenAPIV3.Document
->
+  patchApplyPatch,
+  patchGvk,
+] satisfies Array<(doc: OpenAPIV3.Document) => OpenAPIV3.Document>
